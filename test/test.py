ce175d15458cd0716745c37a88869f11cc21eb90
flypotatojun <ocko@163.com>
2017-10-26 13:29:06 +0800

    modify

--- test/test.py
+++ test/test.py
@@ -1,6 +1,5 @@
-sels =(a,b,c,d)
-for i in sels:
-    if len(i)>2:
-        print hello world!
-    else :
-        print goodbye
+sels =(1,2,3,4)
+if len(sels)>2:
+    print 'hello world!'
+else :
+    print 'goodbye'
